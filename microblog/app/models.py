51c1279c4962cc74116e8e142ebe5bb52e0901c6
Zeaiter Zeaiter <zeaiter.zeaiter@sydney.edu.au>
2021-12-16 22:01:47 +1100

    Not attempted chapter 13 as I need a freemium account

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -28,7 +28,7 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-    
+
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
